7e1e9dbf83d77dbdf1f5e9611106a430a8c7fd20
Saad Bin Akhlaq <saad.18@gmail.com>
2016-06-20 00:19:35 +0530

    Add controller and rendering code

--- lib/kahwa.rb
+++ lib/kahwa.rb
@@ -1,9 +1,28 @@
-require "kahwa/version"
+require 'kahwa/version'
+require 'kahwa/controller'
+require 'kahwa/utils'
 
 module Kahwa
-  class App
+  class Application
     def call(env)
-      [200, { 'Content-type' => 'text/html' }, ['Hello World!!']]
+      if env['PATH_INFO'] == '/'
+        return [302, { 'Location' => '/pages/about' }, []]
+      end
+
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {}, []]
+      end
+
+      contoller_class, action = get_controller_and_action env
+
+      response = contoller_class.new.send(action)
+      [200, { 'Content-type' => 'text/html' }, [response]]
+    end
+
+    def get_controller_and_action(env)
+      _, controller_name, action = env['PATH_INFO'].split('/')
+      controller_name = controller_name.to_camel_case + 'Controller'
+      [Object.const_get(controller_name), action]
     end
   end
 end
